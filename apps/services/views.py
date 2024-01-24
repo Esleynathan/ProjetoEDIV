2dd1b5d7eef9d238bb23c771b773d74076d04eb7
esleynathan <esleynathan@hotmail.com>
2024-01-24 00:15:12 -0300

    07 - Registrando os orçamentos

--- apps/services/views.py
+++ apps/services/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.core.files.storage import default_storage
 from django.core.files.base import ContentFile
 from django.http import HttpResponse, HttpResponseBadRequest
@@ -6,18 +6,45 @@ from django.core.files.uploadedfile import TemporaryUploadedFile, InMemoryUpload
 from .uploaded_file import ChunkUploadedFile
 from .utils import is_video
 from tinymce.widgets import TinyMCE
+from .choices import ServiceTags
+from .models import Budgets
+from datetime import datetime
+from django.contrib import messages
+from django.contrib.messages import constants
+from django.urls import reverse
+from django.utils.safestring import mark_safe
+from django.views import View
 
+class BudgetRequestView(View):
 
-def request_budget(request):
-    if request.method == "GET":
+    @staticmethod
+    def get(request):
         HTML_FIELD = TinyMCE().render(name='descricao', value='', attrs={'id': 'id_descricao'})
-        return render(request, 'request_budget.html', {'HTML_FIELD': HTML_FIELD})
-    elif request.method == "POST":
+        return render(request, 'request_budget.html', {'HTML_FIELD': HTML_FIELD, 'services': ServiceTags.choices})
+    
+    @staticmethod
+    def post(request):
         file = request.FILES.get('file')
+        service = request.POST.get('service')
+        descricao = request.POST.get('descricao')
+
         if not is_video(file):
             raise HttpResponseBadRequest()
         
         file_upload = ChunkUploadedFile(file)
-        file_upload.save_disk()
+        file_path = file_upload.save_disk()
+
+        budget = Budgets(
+            file_path=file_path,
+            data=datetime.now(),
+            service_tag=service,
+            descricao=descricao,
+            cliente=request.user,
+        )
+
+        budget.save()
+        
+        # TODO: Redirecionar para a visualização do orçamento
+        messages.add_message(request, constants.SUCCESS, mark_safe('Orçamento solicitado com sucesso. <a href="#">Clique aqui</a> para ver o status.'))
                             
-        return HttpResponse('teste')
+        return redirect(reverse('request_budget'))
